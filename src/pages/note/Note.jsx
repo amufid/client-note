f7f54e7f4b05d6ab2dd1e1f7923df50cfaf4b94f
amufid <91891129+amufid@users.noreply.github.com>
2024-07-14 08:37:24 +0700

    bug fixing

--- src/pages/note/Note.jsx
+++ src/pages/note/Note.jsx
@@ -128,109 +128,99 @@ export default function Note() {
                            <ModalPinNote />
                         </div>
                      </div>
-                     <div className='mr-2 mb-2'>
-                        <Dropdown label={`${sortBy ? dropdownSort : 'Sort'}`} onChange={() => sortLabel()}>
-                           <Dropdown.Item onClick={() => setSortBy()}>Sort</Dropdown.Item>
-                           <Dropdown.Divider />
-                           <Dropdown.Item onClick={() => setSortBy('createdAt=desc')}>Latest created</Dropdown.Item>
-                           <Dropdown.Item onClick={() => setSortBy('createdAt=asc')}>Oldest created</Dropdown.Item>
-                           <Dropdown.Item onClick={() => setSortBy('updatedAt=desc')}>Latest updated</Dropdown.Item>
-                           <Dropdown.Item onClick={() => setSortBy('updatedAt=asc')}>Oldest updated</Dropdown.Item>
-                        </Dropdown>
-                     </div>
                      <div>
-                        <ModalPinNote />
+                        <FloatingLabel variant="outlined" label="Search title" className='w-54' onChange={e => setSearch(e.target.value)} />
                      </div>
                   </div>
                </div>
             </div>
-         </div>
-         <div className='flex justify-center dark:text-slate-300 text-slate-700 '>
-            <div className='flex flex-col'>
-               {currentNotes.map(note => (
-                  <Card key={note.id} className="w-[370px] sm:w-[550px] mb-1 sm:mb-3 mx-5">
-                     <div className='flex flex-row justify-between'>
-                        <div className='flex flex-col'>
-                           <Link to='/detailNote' state={{ id: note.id }}>
-                              <h2 className='text-lg sm:text-xl tracking-tight hover:dark:text-blue-500 hover:text-blue-400 mr-2'>
-                                 {note.title}
-                              </h2>
-                           </Link>
-                           <div className='mt-2'>
-                              <p className='text-xs'>
-                                 {convertTime(note.created_at)}
-                              </p>
-                           </div>
-                        </div>
-                        <div className='flex flex-row'>
-                           <div className='flex flex-row mr-2'>
-                              <ModalAddImage note={note} refetch={getNotes} />
-                              <ModalUpdateNote note={note} refetch={getNotes} />
-                              <ModalDeleteNote note={note} refetch={getNotes} />
-                           </div>
-                           <Dropdown
-                              renderTrigger={() =>
-                                 <span className='hover:text-blue-500 flex mt-3'>
-                                    <SlOptions />
-                                 </span>}>
-                              <Dropdown.Item>
-                                 <button
-                                    value={note.isPinned}
-                                    onClick={() => handlePin(note.id, note.isPinned)} >
-                                    {note.isPinned === null || note.isPinned === false ? (
-                                       <p className='w-full flex flex-row'><BsPinAngleFill /><span className='ml-3'>Pin note</span></p>
-                                    ) : (
-                                       <p className='w-full flex flex-row'><RiUnpinFill /><span className='ml-3'>Unpin note</span></p>
-                                    )}
-                                 </button>
-                              </Dropdown.Item>
-                           </Dropdown>
-                        </div>
-                     </div>
-                     <pre className='responsive-pre text-sm'>
-                        {note.content}
-                     </pre>
-                     <Link to='/detailNote' state={{ id: note.id }}>
-                        <div className='grid grid-cols-3 justify-center items-center'>
-                           {note.attachment.map((note) => (
-                              <div key={note.id} className='m-1'>
-                                 <img src={note.file_path} alt="image" className='w-[150px] h-auto mb-2' />
-                              </div>
-                           ))}
-                        </div>
-                     </Link>
-                     <div className='flex flex-row justify-between'>
-                        <div>
-                           {note.note_tags.map((note) => (
-                              <div key={note.id} className='mr-2'>
+            <div className='flex justify-center dark:text-slate-300 text-slate-700 '>
+               <div className='flex flex-col'>
+                  {currentNotes.map(note => (
+                     <Card key={note.id} className="w-[370px] sm:w-[550px] mb-1 sm:mb-3 mx-5">
+                        <div className='flex flex-row justify-between'>
+                           <div className='flex flex-col'>
+                              <Link to='/detailNote' state={{ id: note.id }}>
+                                 <h2 className='text-lg sm:text-xl tracking-tight hover:dark:text-blue-500 hover:text-blue-400 mr-2'>
+                                    {note.title}
+                                 </h2>
+                              </Link>
+                              <div className='mt-2'>
                                  <p className='text-xs'>
-                                    Tag : {note.tag.name}
+                                    {convertTime(note.created_at)}
                                  </p>
                               </div>
-                           ))}
-                           {note.note_categories.map((note) => (
-                              <div key={note.id}>
-                                 <p className='text-xs'>
-                                    Category : {note.category.name}
-                                 </p>
+                           </div>
+                           <div className='flex flex-row'>
+                              <div className='flex flex-row mr-2'>
+                                 <ModalAddImage note={note} refetch={getNotes} />
+                                 <ModalUpdateNote note={note} refetch={getNotes} />
+                                 <ModalDeleteNote note={note} refetch={getNotes} />
                               </div>
-                           ))}
+                              <Dropdown
+                                 renderTrigger={() =>
+                                    <span className='hover:text-blue-500 flex mt-3'>
+                                       <SlOptions />
+                                    </span>}>
+                                 <Dropdown.Item>
+                                    <button
+                                       value={note.isPinned}
+                                       onClick={() => handlePin(note.id, note.isPinned)} >
+                                       {note.isPinned === null || note.isPinned === false ? (
+                                          <p className='w-full flex flex-row'><BsPinAngleFill /><span className='ml-3'>Pin note</span></p>
+                                       ) : (
+                                          <p className='w-full flex flex-row'><RiUnpinFill /><span className='ml-3'>Unpin note</span></p>
+                                       )}
+                                    </button>
+                                 </Dropdown.Item>
+                              </Dropdown>
+                           </div>
                         </div>
-                     </div>
-                  </Card>
-               ))}
+                        <pre className='responsive-pre text-sm'>
+                           {note.content}
+                        </pre>
+                        <Link to='/detailNote' state={{ id: note.id }}>
+                           <div className='grid grid-cols-3 justify-center items-center'>
+                              {note.attachment.map((note) => (
+                                 <div key={note.id} className='m-1'>
+                                    <img src={note.file_path} alt="image" className='w-[150px] h-auto mb-2' />
+                                 </div>
+                              ))}
+                           </div>
+                        </Link>
+                        <div className='flex flex-row justify-between'>
+                           <div>
+                              {note.note_tags.map((note) => (
+                                 <div key={note.id} className='mr-2'>
+                                    <p className='text-xs'>
+                                       Tag : {note.tag.name}
+                                    </p>
+                                 </div>
+                              ))}
+                              {note.note_categories.map((note) => (
+                                 <div key={note.id}>
+                                    <p className='text-xs'>
+                                       Category : {note.category.name}
+                                    </p>
+                                 </div>
+                              ))}
+                           </div>
+                        </div>
+                     </Card>
+                  ))}
+               </div>
+            </div>
+            <div className='flex justify-center py-7'>
+               {notes.length > 5 && (
+                  <Pagination
+                     currentPage={currentPage}
+                     totalPages={totalPages}
+                     onPageChangeNext={handlePageChangeNext}
+                     onPageChangePrev={handlePageChangePrev}
+                  />
+               )}
             </div>
          </div>
-         <div className='flex justify-center py-7'>
-            {notes.length > 5 && (
-               <Pagination
-                  currentPage={currentPage}
-                  totalPages={totalPages}
-                  onPageChangeNext={handlePageChangeNext}
-                  onPageChangePrev={handlePageChangePrev}
-               />
-            )}
-         </div>
-      </div>
+      </>
    )
 }
